d0431a70d8b536764cad75a4854cb5be80d49687
Mohammad <m.ganjikh2020@gmail.com>
2024-08-22 11:14:39 +0430

    oop - constroctor

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -13,3 +13,22 @@ Console.WriteLine(
 Console.WriteLine(StaticCalculator.Add(2, 3)); 
 Console.WriteLine(StaticCalculator.Add(2, 4)); 
 StaticCalculator.Add(2, 5);
+/* Issues caused by procedural programming 
+-spaghetti code
+-No way to control who can access methods
+-No separation by levels of abstraction
+-Changes in requirements are hard to implement
+-logic is not easily configurable*/
+/*
+ * OOP is a coding paradigm in which objects containing data and methods play a central role.
+ * We define what data and methods objects of some type will contain by defining classes.
+ * We can gave many objects (instances) of the same class.
+ * code is modular, which makes it easier to maintain, reuse, and modify.
+ * cod is more flexible, and we can adjust its behavior to changing needs easily.
+ * dode is easier to understand.
+ * It's easy to control how some methods and data can be accessed, Which makes the code less error-prone.
+ * Encapsulation.
+ * polymorphism 
+ * Abstracion
+ * inheritance
+ */
--- OOP/Rectangle.cs
+++ OOP/Rectangle.cs
@@ -9,80 +9,86 @@ namespace OOP
 {
     public class Rectangle
     {
-        StaticCalculator.Add(1,2);
+        // StaticCalculator.Add(1,2);
+        public int PublicThing;
+        private int _privatThing;
+        //const fields are implicitly static
+        public const int NumberOfSides = 4;
 
-            //const fields are implicitly static
-            public const int NumberOfSides = 4;
+        //a static property, belonging to the class as a whole
+        // static fields and properties are used when we need to share a single member between all
+        //class instances .
+        public static int CountOfInstances { get; private set; }
+        //never use static fiels or property . it is bad practice and risky . I don't know why it is risky
+        //a static field
+        private static DateTime _firstUsed;
 
-            //a static property, belonging to the class as a whole
-            // static fields and properties are used when we need to share a single member between all
-            //class instances .
-            public static int CountOfInstances { get; private set; }
-            //never use static fiels or property . it is bad practice and risky . I don't know why it is risky
-            //a static field
-            private static DateTime _firstUsed;
+        //a static constructor
+        static Rectangle()
+        {
+            _firstUsed = DateTime.Now;
+        }
 
-            //a static constructor
-            static Rectangle()
+        public Rectangle(int width, int height)
+        {
+            Width = GetLengthOrDefault(width, nameof(Width));
+            _height = GetLengthOrDefault(height, nameof(_height));
+            ++CountOfInstances;
+        }
+
+        //readonly property - can only be set in the constructor
+        public int Width { get; }
+
+        //achieving a similar behavior as properties give with using methods
+        private int _height;
+        public int GetHeight() => _height;
+
+        public void SetHeight(int value)
+        {
+            if (value > 0)
             {
-                _firstUsed = DateTime.Now;
+                _height = value;
             }
+        }
 
-            public Rectangle(int width, int height)
+        private int GetLengthOrDefault(int length, string name)
+        {
+            const int defaultValueIfNonPositive = 1;
+            if (length <= 0)
             {
-                Width = GetLengthOrDefault(width, nameof(Width));
-                _height = GetLengthOrDefault(height, nameof(_height));
-                ++CountOfInstances;
+                Console.WriteLine($"{name} must be a positive number.");
+                return defaultValueIfNonPositive;
             }
+            return length;
+        }
 
-            //readonly property - can only be set in the constructor
-            public int Width { get; }
+        //expression-bodied methods
+        //could not be made static as they use the state of an instance (width and height)
+        public int CalculateCircumference() => 2 * Width + 2 * _height;
 
-            //achieving a similar behavior as properties give with using methods
-            private int _height;
-            public int GetHeight() => _height;
+        public int CalculateArea() => Width * _height;
 
-            public void SetHeight(int value)
-            {
-                if (value > 0)
-                {
-                    _height = value;
-                }
-            }
+        //a get-only, expression-bodied property
+        //properties should never be performance heavy
+        //don't use computed properties
+        public string Description => $"A rectangle with width {Width} " +
+            $"and height {_height}";
 
-            private int GetLengthOrDefault(int length, string name)
-            {
-                const int defaultValueIfNonPositive = 1;
-                if (length <= 0)
-                {
-                    Console.WriteLine($"{name} must be a positive number.");
-                    return defaultValueIfNonPositive;
-                }
-                return length;
-            }
-
-            //expression-bodied methods
-            //could not be made static as they use the state of an instance (width and height)
-            public int CalculateCircumference() => 2 * Width + 2 * _height;
-
-            public int CalculateArea() => Width * _height;
-
-            //a get-only, expression-bodied property
-            //properties should never be performance heavy
-            //don't use computed properties
-            public string Description => $"A rectangle with width {Width} " +
-                $"and height {_height}";
-
-            //a static method, not using any state of an instance
-            public static string DescribeGenerally() =>
-                $"A plane figure with four straight sides and four right angles.";
+        //a static method, not using any state of an instance
+        public static string DescribeGenerally() =>
+            $"A plane figure with four straight sides and four right angles.";
         //Compile time errore
         //public static string DescribeGenerallyWithFields() => $"Sides are : {Width}, {_height} {CalculateArea}";
         //An object reference is required for the non-static field, method, or property 'member'
 
         //can be made static
         public string NotUsingAnyState() => "abc";
-        
+        /*
+         * About properties can make benefits
+         * - avoid entering invalid state of objects of class 
+         * -the implementation details of class, (abstraction)
+         * -property avoid these two issuse
+         */
 
 
     }
